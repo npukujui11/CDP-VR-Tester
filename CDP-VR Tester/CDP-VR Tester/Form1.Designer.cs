14785ffa7037bff2b5cc23e34f9c26b37f7c359d
Ku Jui <npukujui11@gmail.com>
2023-12-08 19:11:47 +0800

    完成btn和slider的设计工作。

---
+++ CDP-VR Tester/CDP-VR Tester/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace CDP_VR_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnDarkness = new System.Windows.Forms.Button();
+            this.btnSeethrough = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(44, 47);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(163, 64);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisconnect.Location = new System.Drawing.Point(268, 47);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(165, 64);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // btnDarkness
+            // 
+            this.btnDarkness.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDarkness.Location = new System.Drawing.Point(44, 241);
+            this.btnDarkness.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDarkness.Name = "btnDarkness";
+            this.btnDarkness.Size = new System.Drawing.Size(163, 64);
+            this.btnDarkness.TabIndex = 2;
+            this.btnDarkness.Text = "Darkness";
+            this.btnDarkness.UseVisualStyleBackColor = true;
+            // 
+            // btnSeethrough
+            // 
+            this.btnSeethrough.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeethrough.Location = new System.Drawing.Point(268, 241);
+            this.btnSeethrough.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSeethrough.Name = "btnSeethrough";
+            this.btnSeethrough.Size = new System.Drawing.Size(165, 64);
+            this.btnSeethrough.TabIndex = 3;
+            this.btnSeethrough.Text = "Seethrough";
+            this.btnSeethrough.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(44, 342);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(163, 64);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Freeze Pitch";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(268, 342);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(165, 64);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Set Pitch";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(44, 462);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(388, 69);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(44, 546);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.trackBar2.Maximum = 3;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(388, 69);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Value = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(44, 145);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(389, 72);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 636);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnSeethrough);
+            this.Controls.Add(this.btnDarkness);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "CDP-VR Tester app";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnDarkness;
+        private System.Windows.Forms.Button btnSeethrough;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ CDP-VR Tester/CDP-VR Tester/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net.Sockets;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net;
+using System.Threading;
+
+namespace CDP_VR_Tester
+{
+    public partial class Form1 : Form
+    {
+        private BluetoothClient bluetoothClient;
+        private BluetoothDeviceInfo vrDevice;
+        private readonly Guid serviceGuid = new Guid("00001101-0000-1000-8000-00805F9B34FB");
+        public Form1()
+        {
+            InitializeComponent();
+            bluetoothClient = new BluetoothClient();
+        }
+
+        private byte[] BuildCommandPacket(bool requestVersion, bool setSeethrough, bool setDarkness, bool setFreezePitch, bool setPitch, bool requestBattery, int scene, int pitch)
+        {
+            byte[] packet = new byte[3];
+            packet[0] = 0;
+            packet[1] = 0;
+            packet[2] = 0;
+
+            // 第 1 位：请求软件版本号
+            if (requestVersion) packet[0] |= 0x80;
+
+            // 第 9 位：设置 Seethrough 模式
+            if (setSeethrough) packet[0] |= 0x01;
+
+            // 第 10 位：设置 Darkness 模式
+            if (setDarkness) packet[1] |= 0x80;
+
+            // 第 11 位：设置 Freeze Pitch 模式
+            if (setFreezePitch) packet[1] |= 0x40;
+
+            // 第 12 位：设置 Set Pitch 模式
+            if (setPitch) packet[1] |= 0x20;
+
+            // 第 13 位：请求电池电量
+            if (requestBattery) packet[1] |= 0x10;
+
+            // 第 14-15 位：设置 VR 场景
+            if (scene >= 0 && scene <= 3) packet[1] |= (byte)(scene << 4);
+
+            // 第 16-21 位：设置 Pitch 值, -10-10
+            if (pitch >= -10 && pitch <= 10) packet[1] |= (byte)(pitch + 10);
+
+            return packet;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 这里需要替换为 VR 眼镜的真实蓝牙地址
+                BluetoothAddress vrDeviceAddress = BluetoothAddress.Parse("VR_DEVICE_BLUETOOTH_ADDRESS");
+                vrDevice = new BluetoothDeviceInfo(vrDeviceAddress);
+
+                bluetoothClient.BeginConnect(vrDevice.DeviceAddress, serviceGuid, new AsyncCallback(ConnectCallback), bluetoothClient);
+
+                btnConnect.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bluetooth connection failed: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult result)
+        {
+            try
+            {
+                bluetoothClient.EndConnect(result);
+
+                // 连接成功，可以发送命令到 VR 眼镜
+                this.Invoke((MethodInvoker)delegate
+                {
+                    richTextBox1.AppendText("Bluetooth connection was successful.\n"); // 在富文本richTextBox1中返回“连接成功”的文本
+                    btnDisconnect.Enabled = true; // 连接成功，可以断开连接
+                });
+            }
+            catch (Exception ex)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    MessageBox.Show($"Bluetooth connection failed: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    btnConnect.Enabled = true; // 连接失败，可以再次尝试连接
+                    btnDisconnect.Enabled = false; // 连接失败，不允许断开连接
+                });
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (bluetoothClient.Connected)
+            {
+                bluetoothClient.Close();
+
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false; // 连接断开，不允许再次断开连接
+                richTextBox1.AppendText("The Bluetooth connection is already cancelled.\n"); // 在富文本richTextBox1中返回“连接已断开”的文本
+            }
+        }
+
+        private void btnDarkness_Click(object sender, EventArgs e)
+        {
+            if (bluetoothClient.Connected)
+            {
+                byte[] command = BuildCommandPacket(false, false, true, false, false, false, 0, 0);
+                bluetoothClient.GetStream().Write(command, 0, command.Length);
+            }
+        }
+
+        private void btnSeethrough_Click(object sender, EventArgs e)
+        {
+            if (bluetoothClient.Connected)
+            {
+                byte[] command = BuildCommandPacket(false, true, false, false, false, false, 0, 0);
+                bluetoothClient.GetStream().Write(command, 0, command.Length);
+            }
+        }
+    }
+}
